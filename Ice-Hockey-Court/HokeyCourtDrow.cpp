d015416791936311cb36089a0bd75bd7ed9da77a
carlos <carlos@debian.org>
2019-02-06 10:26:48 -0300

    testes

--- Ice-Hockey-Court/HokeyCourtDrow.cpp
+++ Ice-Hockey-Court/HokeyCourtDrow.cpp
@@ -91,7 +91,7 @@ void display(void)
 {
  glClear(GL_COLOR_BUFFER_BIT);
  glColor3f(0.0, 1.0, 0.0);
- // drowHockeycourtLineEq(300, 500, 500, 100);
+ drowHockeycourtLineEq(300, 500, 500, 100);
  drowHockeycourtmidPoint(300, 500, 500, 100);
  glFlush();
 }
