b1d4c4908e9f80f16fea3c0fcf64f712a30aa173
carlos <carlos@debian.org>
2019-02-06 08:53:51 -0300

    algoritimos de ponto medio

--- Ice-Hockey-Court/HokeyCourtDrow.cpp
+++ Ice-Hockey-Court/HokeyCourtDrow.cpp
@@ -4,7 +4,7 @@
 
 const int width = 800, height = 600;
 
-void drowHockeycourt(int x1, int y1, int x2, int y2){
+void drowHockeycourtLineEq(int x1, int y1, int x2, int y2){
   if(x1 < x2){
       int aux;aux = x1; x1 = x2; x2 = aux;
   }
@@ -45,12 +45,59 @@ void drowHockeycourt(int x1, int y1, int x2, int y2){
    drowCircunferenceSegmentLineEq(middlex, y1-radiossegment, radiossegment, 4);
 
 }
+void drowHockeycourtmidPoint(int x1, int y1, int x2, int y2){
+  if(x1 < x2){
+      int aux;aux = x1; x1 = x2; x2 = aux;
+  }
+  if(y1 < y2){
+    int aux = y2, y2 = y1; y1 = aux;
+  }
+  int radiossegment = (x1-x2)/10;
+  int middley = (y1+y2)/2, middlex = (x1+x2)/2;
+
+   for(int i = y1-radiossegment; i > y2+radiossegment; i--)
+   {
+     drowPoit(x2, i, 2);
+     drowPoit(x1, i, 2);
+   }
+   // Bordas do Campo
+   drowCircunferenceSegmentLineEq(x2+radiossegment, y1-radiossegment, radiossegment, 2);
+   drowCircunferenceSegmentLineEq(x1-radiossegment, y1-radiossegment, radiossegment, 1);
+   drowCircunferenceSegmentLineEq(x2+radiossegment, y2+radiossegment, radiossegment, 3);
+   drowCircunferenceSegmentLineEq(x1-radiossegment, y2+radiossegment, radiossegment, 4);
+   drowLineMidPoint(x2+radiossegment, y1, x1-radiossegment, y1);
+   drowLineMidPoint(x2+radiossegment, y2, x1-radiossegment, y2);
+   // Linhas Do Campo
+   drowLineMidPoint(x2, y2+radiossegment, x1, y2+radiossegment);
+   drowLineMidPoint(x2, y1-radiossegment, x1, y1-radiossegment);
+   drowLineMidPoint(x2, middley, x1, middley);
+   drowLineMidPoint(x2, middley + (y1-middley)*(0.3), x1, middley + (y1-middley)*(0.3));
+   drowLineMidPoint(x2, middley - (y1-middley)*(0.3), x1, middley - (y1-middley)*(0.3));
+   //CircunferenciasDoCampo
+   // drowMidPointCircle(middlex + (x1-middlex)*0.6, middley - (y1-middley)*(0.6), radiossegment+radiossegment*0.3,);
+   // drowMidPointCircle(middlex - (x1-middlex)*0.6, middley - (y1-middley)*(0.6), radiossegment+radiossegment*0.3);
+   // drowMidPointCircle(middlex - (x1-middlex)*0.6, middley + (y1-middley)*(0.6), radiossegment+radiossegment*0.3);
+   // drowMidPointCircle(middlex + (x1-middlex)*0.6, middley + (y1-middley)*(0.6), radiossegment+radiossegment*0.3);
+   // CirculosCentrais
+   // drowMidPointCircle(middlex, middley, radiossegment+radiossegment*0.5);
+   drowCircunferenceSegmentLineEq(middlex, y2+radiossegment, radiossegment, 1);
+   drowCircunferenceSegmentLineEq(middlex, y2+radiossegment, radiossegment, 2);
+   drowCircunferenceSegmentLineEq(middlex, y1-radiossegment, radiossegment, 3);
+   drowCircunferenceSegmentLineEq(middlex, y1-radiossegment, radiossegment, 4);
+
+}
 
 void display(void)
 {
  glClear(GL_COLOR_BUFFER_BIT);
  glColor3f(0.0, 1.0, 0.0);
- drowHockeycourt(300, 500, 500, 100);
+ // drowHockeycourtLineEq(300, 500, 500, 100);
+ // drowHockeycourtmidPoint(300, 500, 500, 100);
+ drowMidPointCircle(100, 100, 50, 1);
+ drowMidPointCircle(100, 100, 50, 2);
+ drowMidPointCircle(100, 100, 50, 3);
+ drowMidPointCircle(100, 100, 50, 4);
+
  glFlush();
 }
 
@@ -63,7 +110,7 @@ int main(int argc, char **argv)
   glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
     glutInitWindowSize(width, height);
-    // glutInitWindowPosition(0, 0);
+    glutInitWindowPosition(0, 0);
     glutCreateWindow("Input");
     init();
   glutDisplayFunc(display);
--- Ice-Hockey-Court/functions.h
+++ Ice-Hockey-Court/functions.h
@@ -62,3 +62,122 @@ void drowCircunferenceSegmentLineEq(int x, int y, int r, int quadrant){
 void drowFullCircunferenceLineEq(int x, int y, int r){
     for(int i = 1; i  <= 4; i++) drowCircunferenceSegmentLineEq(x, y, r, i);
 }
+
+void drowLineMidPoint(int x1, int y1, int x2, int y2){
+  if(x1 > x2)
+  {
+    int aux = x1; x1 = x2; x2 = aux;
+    aux=y1; y1=y2; y2=aux;
+  }
+  int dx, dy, incE, incNE, d, x, y;
+  dx = x2-x1;
+  dy = y2-y1;
+  d = 2*dy - dx;
+  incE = 2*dy;
+  incNE = 2*(dy-dx);
+  x = x1;
+  y = y1;
+  glPushMatrix();
+  drowPoit(x, y, 2);
+  while(x < x2){
+    if(d <= 0){
+      d += incE;
+      x++;
+    }else{
+      d += incNE;
+      x+=1;
+      y+=1;
+    }
+    drowPoit(x, y, 2);
+  }
+  glPopMatrix();
+}
+void drowMidPointCircle(int x1, int y1, int r, int quadrant){
+  glPushMatrix();
+  glTranslated(x1, y1, 0);
+  if(quadrant == 1){
+    int x, y;
+    float d;
+
+    x = 0;
+    y = r;
+    d = 5/4 - r;
+
+    drowPoit(x, y, 2);
+    while(y > x){
+      if(d < 0){
+        d += 2*x+3;
+        x++;
+      }else{
+        d += 2*(x-y)+5;
+        x++;
+        y--;
+      }
+      drowPoit(x, y, 2);
+    }
+  }
+  if(quadrant == 2){
+    int x, y;
+    float d;
+
+    x = 0;
+    y = r;
+    d = 5/4 - r;
+
+    drowPoit(-x, y, 2);
+    while(y > x){
+      if(d < 0){
+        d += 2*x+3;
+        x++;
+      }else{
+        d += 2*(x-y)+5;
+        x++;
+        y--;
+      }
+      drowPoit(-x, y, 2);
+    }
+  }
+  if(quadrant == 3){
+    int x, y;
+    float d;
+
+    x = 0;
+    y = r;
+    d = 5/4 - r;
+
+    drowPoit(x, -y, 2);
+    while(y > x){
+      if(d < 0){
+        d += 2*x+3;
+        x++;
+      }else{
+        d += 2*(x-y)+5;
+        x++;
+        y--;
+      }
+      drowPoit(x, -y, 2);
+    }
+  }
+  if(quadrant == 4){
+    int x, y;
+    float d;
+
+    x = 0;
+    y = r;
+    d = 5/4 - r;
+
+    drowPoit(-x, -y, 2);
+    while(y > x){
+      if(d < 0){
+        d += 2*x+3;
+        x++;
+      }else{
+        d += 2*(x-y)+5;
+        x++;
+        y--;
+      }
+      drowPoit(-x, -y, 2);
+    }
+  }
+  glPopMatrix();
+}
